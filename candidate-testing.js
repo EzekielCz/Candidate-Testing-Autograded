10b8d9e950b2e27d3f56a068c04965530f4b5fea
EzekielCz <ezechielcazacu@gmail.com>
2023-09-28 19:48:20 -0500

    commit

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,33 +12,40 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
-
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", 
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride","true", "40", "Trajectory", "3"]; 
+let candidateAnswers = [];
+let numCorrectAnswers = 0;
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+  // TODO 1.1b: Ask for candidate's name //"
     candidateName = input.question(" What's your name ? ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-    candidateAnswer = input.question(question);
+    for (let i = 0; i <questions.length; i++){
+      candidateAnswers[i] = input.question(questions[i]);
+      //candidateAnswers.push(input.question(questions[i]))
+    }
 
 }
 
 function gradeQuiz(candidateAnswers) {
+let grade = 0;
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly 
+  for (let i =0; i <correctAnswers.length; i++){
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){  
+        numCorrectAnswers = numCorrectAnswers + 1 
+    }
+  }
+   grade = (numCorrectAnswers) / (questions.length) * 100
+  
+   console.log(`Good job on completeing the test your grade was: ${grade}% `);
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-    console.log("Correct Answer: " + correctAnswer);
-} else { 
-  console.log("Incorrect Answer. Correct Answer " + correctAnswer);
-}
 
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
+ // let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
